baeea0cb889c689af0adc872f8c626cbbe9517dc
CharishmaNagandla <nagandlacharishma2012@gmail.com>
2026-02-23 13:03:47 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+const form = document.getElementById('contact-form');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent page reload
+    const submitButton = form.querySelector('button');
+    submitButton.disabled = true;
+
+    emailjs.sendForm('service_0suit2f', 'template_gu8879x', this)
+      .then(() => {
+          alert('Message sent successfully!');
+          form.reset(); // Clear form
+          submitButton.disabled = false;
+      })
+      .catch((error) => {
+          alert('Oops! Something went wrong: ' + error.text);
+          submitButton.disabled = false;
+      });
+});
